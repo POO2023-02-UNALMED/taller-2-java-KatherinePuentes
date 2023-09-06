4783df204937a00493583fe7ee8cb004b13e6a15
KatherinePuentes <141962369+KatherinePuentes@users.noreply.github.com>
2023-09-06 10:23:03 -0500

    auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -9,7 +9,7 @@ public class Auto {
     int registro;
     static int cantidadCreados;
 
-    public int cantidadAsientos(){
+    int cantidadAsientos(){
         int cantidad=0;
         for (Asiento asiento :asientos){
             if (asiento!=null){
@@ -19,22 +19,7 @@ public class Auto {
         return cantidad;
     }
 
-    public String verificarIntegridad(){
-        if (original()) {
-            return "Auto original";
-        }
-        return "Las piezas no son originales";
-    }
-
-    public boolean original() {
-        if (motor.registro != registro) {
-            return false;
-        }
-        for (Asiento asiento : asientos) {
-            if (asiento != null && asiento.registro != registro) {
-                return false;
-            }
-        }
-        return true;  
+    String verificarIntegridad(){
+        return "";  
     }
 }
